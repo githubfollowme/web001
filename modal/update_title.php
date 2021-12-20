9d40a7e5817daf57938b4118ea0e6f61c221d22f
githubfollowme <flymayday112@gmail.com>
2021-12-20 11:15:27 +0800

    完成在後台顯示網站標題區圖片列表

--- modal/update_title.php
+++ modal/update_title.php
@@ -1,15 +1,17 @@
-<h3>新增標題圖片</h3>
+<h3>更新增標題圖片</h3>
 <hr>
-<form action="api/title.php" method="post" enctype="multipart/form-data">
+<form action="api/update_title.php" method="post" enctype="multipart/form-data">
     <table>
         <tr>
             <td>標題區圖片：</td>
             <td><input type="file" name="img" ></td>
         </tr>
-        <tr>
+        <!-- for--完成在後台顯示網站標題區圖片列表 -->
+        <!-- 這裡決定替代文字不用了? -->
+        <!-- <tr>
             <td>標題區替代文字：</td>
             <td><input type="text" name="text" ></td>
-        </tr>
+        </tr> -->
     </table>
     <div><input type="submit" value="新增"><input type="reset" value="重置"></div>
 </form>
