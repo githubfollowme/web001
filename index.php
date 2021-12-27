7b8d263bf60ee5f6d9a85a4ea6580bfa592a2b53
githubfollowme <flymayday112@gmail.com>
2021-12-27 10:34:50 +0800

    完成管理登入簡易版

--- index.php
+++ index.php
@@ -116,7 +116,8 @@ if(file_exists($file)){
                         </script> -->
                                  <div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
                 	<!--右邊-->   
-                	<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=admin&#39;)">管理登入</button>
+                	<!-- 把do=admin 或do=back.php改成do=login -->
+					<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=login&#39;)">管理登入</button>
                 	<div style="width:89%; height:480px;" class="dbor">
                     	<span class="t botli">校園映象區</span>
 						                        <script>
