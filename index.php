9ddae88b748a610957639255b7483b86e92fe8cf
githubfollowme <flymayday112@gmail.com>
2022-02-07 11:11:48 +0800

    update web001 index

--- index.php
+++ index.php
@@ -1,36 +1,39 @@
 <?php
 include_once "base.php";
 
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
-<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-<title>卓越科技大學校園資訊系統</title>
-<link href="./home_files/css.css" rel="stylesheet" type="text/css">
-<script src="./home_files/jquery-1.9.1.min.js"></script>
-<script src="./home_files/js.js"></script>
+?>
+
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <title>卓越科技大學校園資訊系統</title>
+    <link href="./css/css.css" rel="stylesheet" type="text/css">
+    <script src="./js/jquery-1.9.1.min.js"></script>
+    <script src="./js/js.js"></script>
 </head>
 
 <body>
-<div id="cover" style="display:none; ">
-	<div id="coverr">
-    	<a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;" onclick="cl(&#39;#cover&#39;)">X</a>
-        <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
+    <div id="cover" style="display:none; ">
+        <div id="coverr">
+            <a style="position:absolute; right:3px; top:4px; cursor:pointer; z-index:9999;"
+                onclick="cl(&#39;#cover&#39;)">X</a>
+            <div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
+        </div>
     </div>
-</div>
-
-<!--早期ajax不普及的時候 iframe是個替代方案 但後來發現會有資安考量  -->
-<!-- <iframe style="display:none;" name="back" id="back"></iframe> -->
-	<div id="main">
-    	<a title="" href="./home_files/home.htm"><div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div><!--標題--></a>
-        	<div id="ms">
-             	<div id="lf" style="float:left;">
-            		<div id="menuput" class="dbor">
+    <div id="main">
+    <?php include "front/header.php";?>
+        <div id="ms">
+            <div id="lf" style="float:left;">
+                <div id="menuput" class="dbor">
                     <!--主選單放此-->
-                    	                            <span class="t botli">主選單區</span>
-													<!-- 完成前台選單功能 -->
-													<?php
+                    <span class="t botli">主選單區</span>
+                    <?php
                         $mains=$Menu->all(['parent'=>0,'sh'=>1]);
                         foreach($mains as $main){
                             echo "<div class='mainmu'>";
@@ -52,99 +55,74 @@ include_once "base.php";
 
 
                     ?>
-                                                </div>
-                    <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
-					<span class="t">進站總人數 : <?=$Total->find(1)['total'];?></span>
-                    </div>
-        		</div>
-               
-															<?php 
-					$do=isset($_GET["do"])?$_GET["do"]:'main';
-$file="./front/".$do.".php";
-
-if(file_exists($file)){
-	include $file;
-}else{
-	// echo "檔案不存在";
-	include "./front/main.php";
-}
-
-//$do=$_GET["do"]??'main'  跟65行一樣意思;
-					// if(isset($_GET['do'])){
-					// $do=$_GET['do'];
-					// }else{
-						// $do="main";
-					// }
-					// inlude 檔案
-					?>
 
 
-															<div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
-                    	<script>
-						$(".ssaa li").hover(
-							function ()
-							{
-								$("#altt").html("<pre>"+$(this).children(".all").html()+"</pre>")
-								$("#altt").show()
-							}
-						)
-						$(".ssaa li").mouseout(
-							function()
-							{
-								$("#altt").hide()
-							}
-						)
-                        </script>
-                    </div>
-                	                </div>
-									<!-- 完成更多最新消息顯示 把77到93註銷 -->
-                <!-- <div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
-                    	<script>
-						$(".sswww").hover(
-							function ()
-							{
-								$("#alt").html(""+$(this).children(".all").html()+"").css({"top":$(this).offset().top-50})
-								$("#alt").show()
-							}
-						)
-						$(".sswww").mouseout(
-							function()
-							{
-								$("#alt").hide()
-							}
-						)
-                        </script> -->
-                                 <div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
-                	<!--右邊-->   
-                	<!-- 把do=admin 或do=back.php改成do=login -->
-					<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=login&#39;)">管理登入</button>
-                	<div style="width:89%; height:480px;" class="dbor">
-                    	<span class="t botli">校園映象區</span>
-						                        <script>
-                        	var nowpage=0,num=0;
-							function pp(x)
-							{
-								var s,t;
-								if(x==1&&nowpage-1>=0)
-								{nowpage--;}
-								if(x==2&&(nowpage+1)*3<=num*1+3)
-								{nowpage++;}
-								$(".im").hide()
-								for(s=0;s<=2;s++)
-								{
-									t=s*1+nowpage*1;
-									$("#ssaa"+t).show()
-								}
-							}
-							pp(1)
-                        </script>
-                    </div>
                 </div>
-                            </div>
-             	<div style="clear:both;"></div>
-            	<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-                	<span class="t" style="line-height:123px;"></span>
+                <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
+                    <span class="t">進站總人數 :<?=$Total->find(1)['total'];?></span>
                 </div>
+            </div>
+            <?php
+				
+				$do=isset($_GET["do"])?$_GET["do"]:'main';
+				$file="./front/".$do.".php";
+				if(file_exists($file)){
+					include $file;
+				}else{
+					//echo "檔案不存在";
+					include "./front/main.php";
+				}	
+				?>
+
+            <div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
+                <!--右邊-->
+                <button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;"
+                    onclick="lo(&#39;?do=login&#39;)">管理登入</button>
+                <div style="width:89%; height:480px;" class="dbor">
+                    <span class="t botli">校園映象區</span>
+                        <div class="t" onclick="pp(1)"><img src="icon/up.jpg"></div>
+                        <?php 
+                            $imgs=$Image->all(['sh'=>1]);
+                            foreach($imgs as $key => $img){
+                        ?>
+                        <div class="im cent" id="ssaa<?=$key;?>">
+                            <img src="img/<?=$img['img'];?>" style="width:150px;height:103px;border:3px solid orange;margin:1px">
+                        </div>
+                        <?php 
+                            }
+                        ?>
+                        <div class="t" class="t" onclick="pp(2)"><img src="icon/dn.jpg"></div>
+
+                    <script>
+                    var nowpage = 0,
+                        num = <?=$Image->math("count","*",['sh'=>1]);?>;
+
+                    function pp(x) {
+                        var s, t;
+                        if (x == 1 && nowpage - 1 >= 0) {
+                            nowpage--;
+                        }
+                        if (x == 2 && (nowpage + 3) < num) {
+                            nowpage++;
+                        }
+                        $(".im").hide()
+                        for (s = 0; s <= 2; s++) {
+                            t = s * 1 + nowpage * 1;
+                            $("#ssaa" + t).show()
+                        }
+                    }
+                    pp(1)
+                    </script>
+                </div>
+            </div>
+        </div>
+        <div style="clear:both;"></div>
+        <div
+            style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
+            <span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom'];?></span>
+        </div>
     </div>
 
-</body></html>
+</body>
+
+</html>
---
+++ testtest.php
@@ -0,0 +1,271 @@
+<?php
+date_default_timezone_set("Asia/Taipei");
+session_start();
+// 物件導向 像車子 零件就是變數 功能就是方法
+// class是php一種固定導向的名稱 想要建立物件導向的時候
+class DB{
+    protected $dsn="mysql:host=localhost;
+    charset=utf8;dbname=web001";
+    protected $user="root";
+    protected $pw='';
+    protected $pdo;
+    // 宣告公開變數
+    public $table;
+    public $title;
+    public $button;
+    public $header;
+    public $append;
+    public $upload;
+
+    // 去建構
+    public function __construct($table){
+     
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,$this->user,$this->pw);
+        $this->setStr($table);
+    }
+
+    private function setStr($table){
+        switch($table){
+            case "title";
+                $this->title="網站標題管理";
+                $this->button="新增網站標題圖片";
+                $this->header="網站標題";
+                $this->append="替代文字";
+                $this->upload="網站標題圖片";
+                break;
+            case "ad";
+            $this->title="動態文字廣告管理";
+            $this->button="新增動態文字廣告";
+            $this->header="動態文字廣告";
+            break;
+            
+            case "mvim";
+            $this->title="動畫圖片管理";
+            $this->button="新增動畫圖片";
+            $this->header="動畫圖片";
+            $this->upload="動畫圖片";
+            break;
+            case "image";
+            $this->title="校園映像資料管理";
+            $this->button="新增校園映像圖片";
+            $this->header="校園映像資料圖片";
+            $this->upload="校園映像圖片";
+            break;
+            case "total";
+            $this->title="進站總人數管理";
+            $this->button="";
+            $this->header="進站總人數:";
+            break;
+            case "bottom";
+            $this->title="頁尾版權資料管理";
+            $this->button="";
+            $this->header="頁尾版權資料";
+            break;
+            case "news";
+            $this->title="最新消息資料管理";
+            $this->button="新增最新消息資料";
+            $this->header="最新消息資料內容";
+            break;
+            case "admin";
+            $this->title="管理者帳號管理";
+            $this->button="新增管理者帳號";
+            $this->header="帳號";
+            $this->append="密碼";
+            break;
+            case "menu";
+            $this->title="選單管理";
+            $this->button="新增主選單";
+            $this->header="主選單名稱";
+            $this->append="選單連結網址";
+            break;
+        }
+    }
+// 比方 $Total->find('$id') 就是 $Total用find方法 去total資料表把$id撈出來
+    public function find($id){
+        $sql="SELECT * FROM $this->table WHERE ";
+
+        if(is_array($id)){
+            // $key都是字串 然後js的index 都是 數字
+            foreach($id as $key => $value){
+                $tmp[]="`$key`='$value'";
+            }
+// implode Join array elements with a string 是陣列拆成字串 explode是字串組成陣列
+// sql .就是字串纍加 數字纍加則是加號
+            $sql .= implode(" AND ",$tmp);
+        }else{
+            $sql .= " `id`='$id'";
+        }
+
+        // pdo 為pdo資料庫裏的一個方法 主要crud就靠這個->再用pdo裏的query方法把sql結果 最終用fetch帶出來
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+    public function all(...$arg){
+        $sql="SELECT * FROM $this->table ";
+
+        switch(count($arg)){
+            case 2:
+                foreach($arg[0] as $key => $value){
+                    $tmp[]="`$key`='$value'";
+                }
+
+                $sql .=" WHERE ".implode(" AND ",$arg[0])." ".$arg[1];
+
+            break;
+            case 1:
+                if(is_array($arg[0])){
+                    foreach($arg[0] as $key => $value){
+                        $tmp[]="`$key`='$value'";
+                    }
+                    $sql .= " WHERE ".implode(" AND ",$arg[0]);
+                }else{
+                    // $sql .= $arg[1];
+                    $sql .= $arg[0];
+                    
+                }
+            break;
+        }
+
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function math($method,$col,...$arg){
+        $sql="SELECT $method($col) FROM $this->table ";
+
+        switch(count($arg)){
+            case 2:
+                foreach($arg[0] as $key => $value){
+                    $tmp[]="`$key`='$value'";
+                }
+                // 從arg[0]改成了tmp
+                $sql .=" WHERE ".implode(" AND ",$tmp)." ".$arg[1];
+                // $sql .=" WHERE ".implode(" AND ".$arg[0])." ".$arg[1];
+
+            break;
+            case 1:
+                if(is_array($arg[0])){
+                    foreach($arg[0] as $key => $value){
+                        $tmp[]="`$key`='$value'";
+                    }
+                    $sql .= " WHERE ".implode(" AND ",$tmp);
+                }else{
+                    $sql .= $arg[0];
+                    
+                }
+            break;
+        }
+
+
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    public function save($array){
+        if(isset($array['id'])){
+            //update
+            foreach($array as $key => $value){
+                $tmp[]="`$key`='$value'";
+            }
+            $sql="UPDATE $this->table 
+                     SET ".implode(",",$tmp)."
+                   WHERE `id`='{$array['id']}'";
+        }else{
+            //insert
+
+            $sql="INSERT INTO $this->table (`".implode("`,`",array_keys($array))."`) 
+                                     VALUES('".implode("','",$array)."')";
+        }
+
+       // echo $sql;
+        return $this->pdo->exec($sql);
+    }
+
+    public function del($id){
+        $sql="DELETE FROM $this->table WHERE ";
+
+        if(is_array($id)){
+            foreach($id as $key => $value){
+                $tmp[]="`$key`='$value'";
+            }
+
+            $sql .= implode(" AND ",$tmp);
+        }else{
+            $sql .= " `id`='$id'";
+        }
+
+        return $this->pdo->exec($sql);
+    }
+    public function q($sql){
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+}
+
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+function to($url){
+    header("location:".$url);
+}
+// 直接引用total資料表 到 $Total ,new DB是固定用法 就是要引用這個DB類別的時候 賦予到新變數
+$Total=new DB('total');
+$Bottom=new DB('bottom');
+$Title=new DB('title');
+$Ad=new DB('ad');
+$Mvim=new DB('mvim');
+$Image=new DB('image');
+$News=new DB('news');
+$Admin=new DB('admin');
+$Menu=new DB('menu');
+
+
+//$tt=(isset($_GET['do']))?$_GET['do']:'';
+//$tt=isset($_GET['do'])??'';
+$tt=$_GET['do']??'';
+
+switch($tt){
+    case "ad":
+        $DB=$Ad;
+    break;
+    case "mvim":
+        $DB=$Mvim;
+    break;
+    case "image":
+        $DB=$Image;
+    break;
+    case "total":
+        $DB=$Total;
+    break;
+    case "bottom":
+        $DB=$Bottom;
+    break;
+    case "news":
+        $DB=$News;
+    break;
+    case "admin":
+        $DB=$Admin;
+    break;
+    case "menu":
+        $DB=$Menu;
+    break;
+    default:
+        $DB=$Title;
+    break;
+}
+/* $total=$Total->find(1);
+
+//echo $Total->find(1)['total'];
+echo $total['total'];
+
+print_r($Total->all()); */
+
+if(!isset($_SESSION['total'])){
+    $total=$Total->find(1);
+    $total['total']++;
+    $Total->save($total);
+    $_SESSION['total']=$total['total'];
+}
+
+
+?>
